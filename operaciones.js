47acff17dc65139404e4f458a3fda185fbb21a2e
Alvaro <ap19371937@gmail.com>
2025-03-10 16:33:22 -0500

    Operaciones Listo

--- operaciones.js
+++ operaciones.js
@@ -23,6 +23,7 @@ const borrar = document.getElementById('borrar')
 const limpiar = document.getElementById('limpiar')
 const igual = document.getElementById('igual')
 
+let numeros = []
 //funcion para botones
 function escribir(evento) {
     try {
@@ -45,6 +46,60 @@ function escribir(evento) {
         console.log(error)
     }
 }
+
+function operaciones(evento) {
+    try {
+        let operacion = evento.target
+        if (operacion.textContent == '%') {
+            let numero = pantalla.textContent
+            let resul = numero / 100
+            pantalla.textContent = resul.toString()
+        } else {
+            if (pantalla.textContent != '0') {
+                let numero1 = parseInt(pantalla.textContent)
+                numeros.push(numero1)
+                numeros.push(operacion.textContent)
+                pantalla.textContent = '0'
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function resultado() {
+    try {
+        if (numeros.length != 0) {
+            let numero2 = parseInt(pantalla.textContent)
+            numeros.push(numero2)
+            let operador = numeros[1]
+            let total = 0
+            switch (operador) {
+                case '+':
+                    total = numeros[0] + numeros[2]
+                    pantalla.textContent = total.toString()
+                    break;
+                case '-':
+                    total = numeros[0] - numeros[2]
+                    pantalla.textContent = total.toString()
+                    break;
+                case 'X':
+                    total = numeros[0] * numeros[2]
+                    pantalla.textContent = total.toString()
+                    break;
+                case '/':
+                    total = numeros[0] / numeros[2]
+                    pantalla.textContent = total.toString()
+                    break;
+                default:
+                    console.log('No se realizo ninguna operación')
+            }
+            numeros = []
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
 //eventos de click
 
 numbre_0.addEventListener('click', escribir)
@@ -61,3 +116,11 @@ numbre_9.addEventListener('click', escribir)
 limpiar.addEventListener('click', () => {
     pantalla.textContent = 0
 })
+
+suma.addEventListener('click', operaciones)
+resta.addEventListener('click', operaciones)
+multiplicacion.addEventListener('click', operaciones)
+division.addEventListener('click', operaciones)
+porcentaje.addEventListener('click', operaciones)
+
+igual.addEventListener('click', resultado)
